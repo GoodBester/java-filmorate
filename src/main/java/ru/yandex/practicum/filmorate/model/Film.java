36041f40161bb5d64e07698e76c2bc8f62a9a8eb
disov <disov.yurama@gmail.com>
2023-05-22 23:11:43 +0300

    feat: добавить два контроллера(FilmController, UserController) в приложение

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,11 +3,12 @@ package ru.yandex.practicum.filmorate.model;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
+
 import java.time.LocalDate;
 
 @Data
 @Builder
-public class Film{
+public class Film {
     private int id;
     @NotNull
     @NotBlank
